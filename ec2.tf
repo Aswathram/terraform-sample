56f0b3ec52a8558c86df7ef6bd583f2609b195e9
Aswathram <41834516+Aswathram@users.noreply.github.com>
2020-02-09 22:07:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,15 @@
+terraform
+{
+ backend "s3"{
+   bucket ="terraform-aswath"
+   key="terraform.tfstate"
+   region="us-east-1"
+
+provider "aws" {
+region = "us-east-1"
+}
+
+
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
